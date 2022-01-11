ea660273dd5d468644e069c08aed8a8663708a68
geekhall <174821300@qq.com>
2022-01-11 21:33:51 +0800

    20220111-213351

--- src/main/java/cn/geekhall/datastructures/binarytree/NoRecursiveTraversalBT.java
+++ src/main/java/cn/geekhall/datastructures/binarytree/NoRecursiveTraversalBT.java
@@ -9,14 +9,6 @@ import java.util.Stack;
  *
  */
 public class NoRecursiveTraversalBT {
-    public static class Node {
-        public int value;
-        public Node left;
-        public Node right;
-        public Node(int value) {
-            this.value = value;
-        }
-    }
 
     /**
      * 非递归先序遍历二叉树
@@ -216,19 +208,7 @@ public class NoRecursiveTraversalBT {
         return max;
     }
     public static void main(String[] args) {
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-        Node n5 = new Node(5);
-        Node n6 = new Node(6);
-        Node n7 = new Node(7);
-        n1.left = n2;
-        n1.right = n3;
-        n2.left = n4;
-        n2.right = n5;
-        n3.left = n6;
-        n3.right = n7;
+        Node n1 = TreeUtils.generateBasicTree();
         pre(n1);
         System.out.println("=========");
         pos(n1);
