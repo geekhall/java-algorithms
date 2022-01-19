f058b8f5508eb8da2ab9075cdfdee383ef752455
geekhall <174821300@qq.com>
2022-01-19 22:47:59 +0800

    dp

--- src/main/java/cn/geekhall/algorithms/bruteforce/KnapsackProblem.java
+++ src/main/java/cn/geekhall/algorithms/bruteforce/KnapsackProblem.java
@@ -41,6 +41,7 @@ public class KnapsackProblem {
         }
         return Math.max(p1, p2);
     }
+    
     public static int getMaxValue2(int[] w, int[] v, int bag) {
         return process(w, v, 0, bag);
     }
@@ -67,6 +68,8 @@ public class KnapsackProblem {
         }
         return Math.max(p1, p2);
     }
+
+    public
 
     public static void main(String[] args) {
         int[] weight = new int[]{1,4,5,6,8};
