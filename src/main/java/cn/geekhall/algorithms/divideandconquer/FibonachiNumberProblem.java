401f3b5c05ac37bb89a81eb47c195df05d971847
geekhall <174821300@qq.com>
2022-01-26 23:32:10 +0800

    20220126-233210

--- src/main/java/cn/geekhall/algorithms/divideandconquer/FibonachiNumberProblem.java
+++ src/main/java/cn/geekhall/algorithms/divideandconquer/FibonachiNumberProblem.java
@@ -6,6 +6,9 @@ package cn.geekhall.algorithms.divideandconquer;
  * @author yiny
  */
 public class FibonachiNumberProblem  {
+    private static final int[][] BASE = new int[][]{{1,1}, {1,0}};
+    private static final int[][] ZERO = new int[][]{{0,0}, {0,0}};
+
 
     /**
      *
@@ -18,7 +21,8 @@ public class FibonachiNumberProblem  {
      * @return
      */
     public static int fibonachi1(int n) {
-        return 0;
+        int[][] matrix = fb(n);
+        return matrix[0][1];
     }
 
     /**
@@ -28,16 +32,53 @@ public class FibonachiNumberProblem  {
      * @return
      */
     public static int fibonachi2(int n) {
-        if (n == 1 || n == 2) {
-            return 1;
-        }
+        if (n == 1 || n == 2) return 1;
         return fibonachi2(n - 1 ) + fibonachi2(n - 2);
     }
 
+    private static int[][] fb(int n) {
+        if (n == 0) {
+            return ZERO;
+        }
+        if (n == 1) {
+            return BASE;
+        }
+
+        if ((n & 1) == 0) { // 奇数
+            int[][] matrix = fb(n >> 1);
+            return matrixMultiply(matrix, matrix);
+        }
+
+        int[][] matrix = fb((n - 1) >> 1);
+        return matrixMultiply(matrixMultiply(matrix, matrix), BASE);
+    }
+
+    public static int[][] matrixMultiply(int[][] m, int[][] n) {
+        int row = m.length;
+        int col = n[0].length;
+        int[][] r = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                r[i][j] = 0;
+                for (int k = 0; k < m[i].length; k++) {
+                    r[i][j] += m[i][k] + n[k][j];
+                }
+            }
+        }
+        return r;
+    }
+
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        System.out.println(fibonachi2(40));
+        System.out.println(fibonachi1(4));
         long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+        System.out.println("===================");
+        start = System.currentTimeMillis();
+        System.out.println(fibonachi2(4));
+        end = System.currentTimeMillis();
         System.out.println(end - start);
 
     }
